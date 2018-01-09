6cc44d806e994fdcf7d196e930a7ca02143ae03e
Marino Missiroli <marino.missiroli@desy.de>
2018-01-09 09:14:21 +0100

    undone changes to RunPSO.py

--- RunPSO.py
+++ RunPSO.py
@@ -1,25 +1,16 @@
 #!/usr/bin/env python
-import os, sys
-
-from subprocess import call
-
 from PSO.PSOManager import PSOManager
+import sys
+from subprocess import call
 
 if len(sys.argv)<2:
   print "usage:"
-  print "python RunPSO.py [OPTION] FILE_WITH_PSO_CONFIG"
+  print "python PSO.py [OPTION] FILE_WITH_PSO_CONFIG"
   print "[-t] skip tree creating step, for example if you already did it for a previous run" 
   print "[-v] print additional debug information" 
-
-  raise SystemExit
-
+  exit(0)
+  
 currentPath = sys.path[0]+"/"
-
-if os.path.isdir(currentPath+'/Particles'):
-   raise RuntimeError('"Particles" directory already exists')
-else:
-   print('Created dir '+currentPath+'/Particles')
-   call(['mkdir', '-p', currentPath+'/Particles'])
 
 MakeNewTrees=True
 Verbose=False
