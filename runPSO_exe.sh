4bdd80aa9a635da07da82e61fb751dd20f2833e7
Marino Missiroli <marino.missiroli@desy.de>
2018-01-12 17:06:14 +0100

    added PSOConfig_boostedHbb_step7_cate[5,6].txt

--- runPSO_exe.sh
+++ runPSO_exe.sh
@@ -1,10 +1,31 @@
 #!/bin/bash
 
-ODIR="$1"
+set -e
+
+if [ "$#" -eq 1 ]; then
+
+  ODIR="$1"
+
+else
+
+  printf "\n>>> ERROR -- invalid list of command-line argument(s):\n"
+  printf   "             [1] path to output directory\n\n"
+  exit
+
+fi
+
+if [ -d "${ODIR}" ]; then
+
+  printf "\n>>> ERROR -- target output directory already exists: ${ODIR}\n\n"
+  exit
+
+fi
+
+###
 
 mkdir -p "${ODIR}"
 
-for i_categ in `seq 7 7`; do
+for i_categ in `seq 5 7`; do
 
   nohup ./runPSO.py \
     -c     PSOConfig_boostedHbb_step7_cate"${i_categ}".txt \
@@ -13,5 +34,7 @@ for i_categ in `seq 7 7`; do
 
 done
 unset -v i_categ
+
+###
 
 unset -v ODIR
